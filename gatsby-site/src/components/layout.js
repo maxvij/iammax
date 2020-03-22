ac35973e329b1c45f0de037028024eb6977b7b06
Max van IJsselmuiden <max@label305.com>
2020-03-22 19:58:28 +0100

    Added 'gatsby-plugin-sass' dependency.

--- gatsby-site/src/components/layout.js
+++ gatsby-site/src/components/layout.js
@@ -10,7 +10,10 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+
+// CSS imports
 import "./layout.css"
+import "./bootstrap.scss"
 import "./style.css"
 import "./fonts.css"
 import "./sections/intro.css"
