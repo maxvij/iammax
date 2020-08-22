d05ef988b8882b254c44c3fe2fbb59ac76803773
Max van IJsselmuiden <maxvanijsselmuiden@hotmail.com>
2020-08-22 17:59:40 +0200

    Added SVG iPhone frame and link and button styling.

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -37,22 +37,26 @@ class IndexPage extends React.Component {
                         <div className="col-12">
                             <div className="case-panel">
                                 <div className="row">
-                                    <div className="col-12 col-md-6">
+                                    <div className="col-12 col-sm-6">
                                         <div className="iphone-panel waterrower-video">
-                                            <video autoPlay={true} src={WaterRowerVideo} />
-                                            <div className="iphone-frame"></div>
+                                            <video autoPlay="autoplay" loop={true} muted src={WaterRowerVideo} playsinline={true} />
+                                            <div className="iphone-frame"> </div>
                                         </div>
                                     </div>
-                                    <div className="col-12 col-md-6">
-                                        <h3>Refreshing home rowing experience.</h3>
+                                    <div className="col-12 col-sm-6">
+                                        <h3>A refreshing home rowing experience.</h3>
                                         <p className="company">Project at Label305</p>
                                         <p className="paragraph">
                                             The stylish rowing machines from <a href={''}>WaterRower</a> provide their customers with a unique experience. To further improve this experience, the WaterRower monitor can be connected to the WaterRower Connect app, turning a smartphone/tablet into a real-time performance monitor.
                                         </p>
                                         <p className="paragraph">
-                                            For this project, I designed the WaterRower Connect application from scratch. Following a iterative process, a creative, modern and refreshing user interface came to life.
+                                            For this project, I designed the WaterRower Connect application for phone, tablet (iOS and Android) and Apple Watch. A creative and refreshing user experience came to life.
+                                            My personal goal for this project was to create a consistent flow throughout the application, providing a fluent and high-quality user experience.
                                         </p>
-                                        <a className="btn btn-primary btn-video">Watch video</a>
+                                        <div className="btn-container">
+                                            {/*<a className="btn btn-primary btn-video">Watch video</a>*/}
+                                            <a className="btn btn-secondary" href="https://305.nl/en/portfolio/waterrower-connect-apps/" target="_blank" title="Label305 - Case: Apps for WaterRower">View case at 305.nl</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
