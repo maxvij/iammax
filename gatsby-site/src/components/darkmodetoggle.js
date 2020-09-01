5e48a33dc8dbcc36663a2b414dd0f25d8bbe1658
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2020-09-01 20:12:08 +0200

    Added Lottie animation for light/dark mode switch (#6)
    * Added react-lottie package.
    
    * Added dark light Lottie toggle animation.
    
    * Right-aligned the dark mode switch.

--- gatsby-site/src/components/darkmodetoggle.js
+++ gatsby-site/src/components/darkmodetoggle.js
@@ -1,17 +1,39 @@
 import React from "react"
+import Lottie from "react-lottie";
 
-const DarkModeToggle = (props) => {
-  // If the `onClick` prop exists, call it with 'light'
-  const handleClick = () => props.darkMode.value ? (props.onClick && props.onClick('light')) : (props.onClick && props.onClick('dark'));
+export default class DarkModeToggle extends React.Component {
 
-  return (
-      <div className="switch-wrapper">
-        <label className="switch" htmlFor="checkbox">
-          <input type="checkbox" id="checkbox" onChange={handleClick} checked={(props.darkMode.value)}/>
-          <div className="slider round"> </div>
-        </label>
-      </div>
-  )
+    constructor(props) {
+        super(props)
+        this.state = {
+            isStopped: false,
+            isPaused: false,
+            direction: 1
+        }
+    }
+
+
+    render() {
+        const defaultOptions = {
+            loop: false,
+            autoplay: this.props.darkMode.value,
+            animationData: require('../animations/DarkLightToggle.json'),
+            rendererSettings: {
+                preserveAspectRatio: 'xMidYMid slice'
+            }
+        };
+
+        // If the `onClick` prop exists, call it with 'light'
+        const
+            handleClick = () => this.props.darkMode.value ? (this.props.onClick && this.props.onClick('light')) : (this.props.onClick && this.props.onClick('dark'));
+        return <div className="switch-wrapper" onClick={handleClick}>
+            <Lottie options={defaultOptions}
+                    height={32}
+                    width={64}
+                    isStopped={this.state.isStopped}
+                    isPaused={this.state.isPaused}
+                    direction={this.props.darkMode.value ? 1 : -1}
+            />
+        </div>
+    }
 }
-
-export default DarkModeToggle;
