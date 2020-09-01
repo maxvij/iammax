d1b9ffe0b82d36d4de179b7c2fd431916e3f0050
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2020-09-01 22:39:57 +0200

    Added fixed header.

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -43,12 +43,14 @@ class IndexPage extends React.Component {
                                     <Lottie options={introAnim}
                                             height={117}
                                             width={384}
+                                            speed={0.5}
                                     />
                                 </div>
                                 <div className="anim-dark">
                                     <Lottie options={introAnimDark}
                                             height={117}
                                             width={384}
+                                            speed={0.5}
                                     />
                                 </div>
                             </div>
