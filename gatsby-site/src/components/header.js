7f199c7c5593eae977332d03577899a464a8fa8b
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2020-09-02 15:46:28 +0200

    Updated header.

--- gatsby-site/src/components/header.js
+++ gatsby-site/src/components/header.js
@@ -1,17 +1,14 @@
-import {Link} from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import DarkModeToggle from "./darkmodetoggle";
 
-const Header = ({siteTitle, handleTheme, darkMode}) => (
+const Header = ({handleTheme, darkMode}) => (
   <header>
     <div className="container">
       <div className="row">
         <div className="col-8">
           <h1 style={{margin: 0}}>
-            <Link to="/">
-              {siteTitle}
-            </Link>
+            Max van IJsselmuiden
           </h1>
         </div>
         <div className="col-4">
