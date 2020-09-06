4b1c8023bd3694ad5fd42656e47433e5f580f37d
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2020-09-06 11:53:47 +0200

    Removed animations from intro.

--- gatsby-site/src/components/intro.js
+++ gatsby-site/src/components/intro.js
@@ -1,52 +1,18 @@
 import React from "react"
 import Max from "../images/max-bw.png";
-import Lottie from "react-lottie";
 
 const Intro = (props) => {
-    const introAnim = {
-        loop: true,
-        autoplay: true,
-        animationData: require('../animations/Intro.json'),
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
-    };
-
-    const introAnimDark = {
-        loop: true,
-        autoplay: true,
-        animationData: require('../animations/Intro-white.json'),
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
-    };
-
     return (
         <section className="intro">
             <div className="container">
                 <div className="row">
-                    <div className="col-12">
-                        <h1>I'm Max.</h1>
-                        <div className="animation-wrapper">
-                            <div className="anim-light">
-                                <Lottie options={introAnim}
-                                        height={117}
-                                        width={384}
-                                        isClickToPauseDisabled={true}
-                                />
-                            </div>
-                            <div className="anim-dark">
-                                <Lottie options={introAnimDark}
-                                        height={117}
-                                        width={384}
-                                        isClickToPauseDisabled={true}
-                                />
-                            </div>
-                        </div>
+                    <div className="col-10 offset-1 col-sm-6 offset-sm-0 col-lg-5">
+                        <h1>Hi, I'm Max,</h1>
+                        <p className="subtitle">
+                            a creative <span>UX Designer</span>, specialized in <span>app design</span> and <span>animations</span>. <br />
+                        </p>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-12 offset-0 col-md-4 offset-md-4">
+                    <div className="col-12 col-sm-6 col-lg-4 offset-lg-3">
                         <div className="profile-avatar">
                             <img src={Max} alt="Me."/>
                         </div>
