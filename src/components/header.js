8967e6bb1108a147c117ccee6eb4152d901e9e9a
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2020-09-09 14:16:17 +0200

    Optimized header for mobile.

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,31 @@
 import PropTypes from "prop-types"
 import React from "react"
 import DarkModeToggle from "./darkmodetoggle";
+import { Link } from "gatsby";
 
 const Header = ({handleTheme, darkMode}) => (
-  <header>
-    <div className="container">
-      <div className="row">
-        <div className="col-8">
-          <h1 style={{margin: 0}}>
-            Max van IJsselmuiden
-          </h1>
+    <header>
+        <div className="container">
+            <div className="row">
+                <div className="col-8">
+                    <Link to="/"><h1 style={{margin: 0}}>
+                        Max <span>van IJsselmuiden</span>
+                    </h1></Link>
+                </div>
+                <div className="col-4">
+                    <DarkModeToggle darkMode={darkMode} onClick={handleTheme}/>
+                </div>
+            </div>
         </div>
-        <div className="col-4">
-          <DarkModeToggle darkMode={darkMode} onClick={handleTheme} />
-        </div>
-      </div>
-    </div>
-  </header>
+    </header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+    siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+    siteTitle: ``,
 }
 
 export default Header
