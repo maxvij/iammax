d8c5f23a4563d1b2797a9b257140fdec9f51efd4
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2021-07-19 15:05:56 +0200

    Removed contact form.

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,7 +11,7 @@ const Contact = (props) => {
                 <div className="row">
                     <div className="col-12">
                         <h2>Get in touch.</h2>
-                        <p className="subtitle">Feel free to contact me anytime</p>
+                        <p className="subtitle">Feel free to contact me anytime on LinkedIn</p>
                     </div>
                 </div>
                 <div className="row">
@@ -22,7 +22,7 @@ const Contact = (props) => {
                 <div className="row">
                     <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
                         <div className="row">
-                            <div className="col-12 col-md-6">
+                            <div className="col-12 col-md-6 offset-md-3">
                                 <div className="panel">
                                     <img src={Max} alt="Me." className="avatar" loading="lazy"/>
                                     <h3>Max van IJsselmuiden</h3>
@@ -30,51 +30,7 @@ const Contact = (props) => {
                                     <div className="contact-container">
                                         <a href="https://www.linkedin.com/in/maxvanijsselmuiden/"
                                                target="_blank" rel="noreferrer" aria-label="LinkedIn" title="LinkedIn profile"><LogoLinkedin /></a>
-                                        <a href="https://www.twitter.com/maxvij" aria-label="Twitter" title="Twitter profile" target="_blank" rel="noreferrer"><LogoTwitter /></a>
                                     </div>
-                                </div>
-                            </div>
-                            <div className="col-12 col-md-6">
-                                <div className="panel">
-                                    <form id="contactForm"
-                                          action="https://getform.io/f/20abbcd1-0d8c-4752-914c-226890f551be"
-                                          method="POST">
-                                        <div className="row">
-                                            <div className="col-12">
-                                                <div className="form-group">
-                                                    <label htmlFor="name">Name</label>
-                                                    <input id="name" name="name" type="text"
-                                                           className="form-control" placeholder="Barack"/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-12">
-                                                <div className="form-group">
-                                                    <label htmlFor="email">E-mail address</label>
-                                                    <input id="email" name="email" type="email"
-                                                           className="form-control" placeholder="b.obama@whitehouse.gov"/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-12">
-                                                <div className="form-group">
-                                                    <label htmlFor="message">Message</label>
-                                                    <textarea id="message" name="message" className="form-control" placeholder="Hey Max! Long time no see."/>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-12">
-                                                <div className="button-container">
-                                                    <button type="submit"
-                                                            className="btn btn-primary btn-icon btn-animate-right btn-icon-right btn-send">Send<IosArrowForward />
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
                                 </div>
                             </div>
 
