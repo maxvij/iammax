608c2228cb9de052f7956b25c292e9236822b329
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2021-08-31 09:09:41 +0200

    Removed resume button.

--- src/components/about.js
+++ src/components/about.js
@@ -28,9 +28,6 @@ const About = (props) => {
                                 design. My experience has transformed me into a confident, fully-fledged UX Designer,
                                 ready to take on new challenges.
                             </p>
-                            <div className="btn-container center">
-                                <a href="https://www.maxvanijsselmuiden.nl/downloads/Max-van-IJsselmuiden-CV.pdf" download="http://www.maxvanijsselmuiden.nl/downloads/Max-van-IJsselmuiden-CV.pdf" className="btn btn-center btn-primary btn-icon btn-animate-down"><IosCloudDownload />Download resumé</a>
-                            </div>
                         </div>
                     </div>
                     <div className="col-12 col-lg-6">
